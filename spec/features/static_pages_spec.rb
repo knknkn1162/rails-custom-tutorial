2abe308792a0b4bd5c879cfa2cf5e1a20e5e0f6a
knknkn1162 <knknkn1162@gmail.com>
2018-01-05 21:58:32 +0900

    fix routes and refactor test

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.feature "StaticPages", type: :feature do
   subject(:base_title) { 'Ruby on Rails Tutorial Sample App' }
   scenario 'should have the content Sample App' do
-    visit '/static_pages/home'
+    visit root_url
     expect(page).to have_content('Sample App')
   end
 
   scenario 'should have the title Home' do
-    visit '/static_pages/home'
+    visit static_pages_home_url
     expect(page).to have_title "Home | #{base_title}"
   end
 
   scenario 'should have the title about' do
-    visit '/static_pages/about'
+    visit static_pages_about_url
     expect(page).to have_title "About | #{base_title}"
   end
 
   scenario 'should have the title help' do
-    visit '/static_pages/help'
+    visit static_pages_help_url
     expect(page).to have_title "Help | #{base_title}"
   end
 end
