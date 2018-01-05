a3fb5bff46087aa5e7ffd6bd4debd27c353f6604
knknkn1162 <knknkn1162@gmail.com>
2018-01-05 20:41:21 +0900

    refactor static_pages_test

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "StaticPages", type: :feature do
+  subject(:base_title) { 'Ruby on Rails Tutorial Sample App' }
   scenario 'should have the content Sample App' do
     visit '/static_pages/home'
     expect(page).to have_content('Sample App')
@@ -8,16 +9,16 @@ RSpec.feature "StaticPages", type: :feature do
 
   scenario 'should have the title Home' do
     visit '/static_pages/home'
-    expect(page).to have_title 'Home | Ruby on Rails Tutorial Sample App'
+    expect(page).to have_title "Home | #{base_title}"
   end
 
   scenario 'should have the title about' do
     visit '/static_pages/about'
-    expect(page).to have_title 'About | Ruby on Rails Tutorial Sample App'
+    expect(page).to have_title "About | #{base_title}"
   end
 
   scenario 'should have the title help' do
     visit '/static_pages/help'
-    expect(page).to have_title 'Help | Ruby on Rails Tutorial Sample App'
+    expect(page).to have_title "Help | #{base_title}"
   end
 end
