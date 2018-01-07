4d4b23c1c79f2574aca21bf29922bd5e925e3971
knknkn1162 <knknkn1162@gmail.com>
2018-01-07 17:32:59 +0900

    modify help_path, about_path, contact_path

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,27 +3,32 @@ require 'rails_helper'
 RSpec.feature "StaticPages", type: :feature do
   subject(:base_title) { 'Ruby on Rails Tutorial Sample App' }
   scenario 'should have the content Sample App' do
-    visit root_url
+    visit root_path
     expect(page).to have_content('Sample App')
-  end
-
-  scenario 'should have the title Home' do
-    visit static_pages_home_url
     expect(page).to have_title "Home | #{base_title}"
+
+    # in the logo and the left of the header
+    expect(page).to have_link, href: root_path, count: 2
+    # in the header
+    expect(page).to have_link, href: help_path
+    # in the footer
+    expect(page).to have_link, href: about_path
+    # in the footer
+    expect(page).to have_link, href: contact_path
   end
 
   scenario 'should have the title about' do
-    visit static_pages_about_url
+    visit about_path
     expect(page).to have_title "About | #{base_title}"
   end
 
   scenario 'should have the title help' do
-    visit static_pages_help_url
+    visit help_path
     expect(page).to have_title "Help | #{base_title}"
   end
 
   scenario 'should have the title help' do
-    visit static_pages_contact_url
+    visit contact_path
     expect(page).to have_title "Contact | #{base_title}"
   end
   
