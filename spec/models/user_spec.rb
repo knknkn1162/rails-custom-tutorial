7965947cf7a8ab9ade18903978ac74101aeec10b
knknkn1162 <knknkn1162@gmail.com>
2018-01-08 14:30:24 +0900

    update User validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,37 @@ RSpec.describe User, type: :model do
       user = build(:user)
       expect(user).to be_valid
     end
+    
+    it 'name should be present' do
+      user = build(:user, name: ' ')
+      expect(user).not_to be_valid
+    end
+
+    it 'email validation should accept valid address' do
+      valid_addresses = %w[
+        user@example.com
+        USER@foo.COM
+        A_US-ER@foo.bar.org
+        first.last@foo.jp
+        alice+bob@baz.cn
+      ]
+      valid_addresses.each do |valid_address|
+        user = build(:user, email: valid_address)
+        expect(user).to be_valid
+      end
+    end
+
+    it 'email validation should reject invalid address' do
+      invalid_addresses = %w[
+        user@example,com
+        user_at_foo.org
+        user.name@exapmple.foo@bar_baz.com
+        foo@bar+baz.com
+      ]
+      invalid_addresses.each do |invalid_address|
+        user = build(:user, email: invalid_address)
+        expect(user).not_to be_valid
+      end
+    end
   end
 end
