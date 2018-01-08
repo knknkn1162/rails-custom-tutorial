a51a093c8ae70ccb292bd007f006cb96b5e4705c
knknkn1162 <knknkn1162@gmail.com>
2018-01-08 13:49:28 +0900

    test user validation using factory_bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context ':user should be valid' do
+    it 'shold be valid' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
 end
