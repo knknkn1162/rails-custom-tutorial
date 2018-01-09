a76bffa4178ca9927f4b3311d443556174888d60
knknkn1162 <knknkn1162@gmail.com>
2018-01-09 13:14:09 +0900

    delete view test in feature dir2

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,24 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "StaticPages", type: :feature do
-  subject(:base_title) { 'Ruby on Rails Tutorial Sample App' }
-  scenario 'should have the content Sample App' do
-    visit root_path
-    expect(page).to have_content('Sample App')
-    expect(page).to have_title "Home | #{base_title}"
-  end
-
-  scenario 'should have the proper link in home' do
-    visit root_path
-    # in the logo and the left of the header
-    expect(page).to have_link href: root_path, count: 2
-    # in the header
-    expect(page).to have_link href: help_path
-    # in the footer
-    expect(page).to have_link href: about_path
-    # in the footer
-    expect(page).to have_link href: contact_path
-  end
 
   scenario 'should link home to signup page' do
     visit root_path
@@ -26,19 +8,4 @@ RSpec.feature "StaticPages", type: :feature do
     expect(current_path).to eq signup_path
   end
 
-  scenario 'should have the title about' do
-    visit about_path
-    expect(page).to have_title "About | #{base_title}"
-  end
-
-  scenario 'should have the title help' do
-    visit help_path
-    expect(page).to have_title "Help | #{base_title}"
-  end
-
-  scenario 'should have the title help' do
-    visit contact_path
-    expect(page).to have_title "Contact | #{base_title}"
-  end
-  
 end
