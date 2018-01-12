d10eacde96060fc8fe572147ac10a051486ffed5
knknkn1162 <knknkn1162@gmail.com>
2018-01-12 23:13:58 +0900

    introduce remember_me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save :downcase_email
   validates :name,
     presence: true,
@@ -15,10 +16,27 @@ class User < ApplicationRecord
   # you can use password & password_confirmation attribute
   # and authenticate method
   has_secure_password
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 
   private
