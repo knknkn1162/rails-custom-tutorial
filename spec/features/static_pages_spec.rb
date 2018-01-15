7a9b03657c3e8ccfff1c7d63ecbfc659d940cdc3
knknkn1162 <knknkn1162@gmail.com>
2018-01-15 11:18:06 +0900

    test pagination for users_path

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -8,35 +8,4 @@ RSpec.feature "StaticPages", type: :feature do
       expect(current_path).to eq signup_path
     end
   end
-
-  describe 'visit users_path' do
-    scenario 'should have the proper link as login user' do
-      user = create(:user)
-      visit login_path
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
-
-      click_button 'Log in'
-
-      visit users_path
-      # in the logo and the left of the header
-      expect(page).to have_link href: root_path, count: 2
-      # in the header
-      expect(page).to have_link href: help_path
-      # in the footer
-      expect(page).to have_link href: about_path
-      # in the footer
-      expect(page).to have_link href: contact_path
-      expect(page).to have_selector('ul>li>img')
-      expect(page).to have_link href: user_path(user), text: user.name
-    end
-
-    scenario 'should redirect to login_url as non-login user' do
-      user = create(:user)
-      # without log-in
-      visit users_path
-
-      expect(current_path).to eq login_path
-    end
-  end
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -13,20 +13,64 @@ RSpec.feature "Users", type: :feature do
     expect(page).to have_selector('div.alert')
   end
 
-  scenario 'login with valid information' do
-    user = create(:user)
-    visit login_path
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
+  describe 'visit user_path' do
+    scenario 'login with valid information' do
+      user = create(:user)
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_link(href: login_path, count: 0)
+      expect(page).to have_link(href: logout_path)
+      expect(page).to have_link(href: user_path(user))
 
-    expect(page).to have_link(href: login_path, count: 0)
-    expect(page).to have_link(href: logout_path)
-    expect(page).to have_link(href: user_path(user))
+      click_link 'Log out'
+      expect(current_path).to eq root_path
+      expect(page).to have_link(href: login_path)
+      expect(page).to have_link(href: logout_path, count: 0)
+      expect(page).to have_link(href: user_path(user), count: 0)
+    end
+  end
 
-    click_link 'Log out'
-    expect(page).to have_link(href: login_path)
-    expect(page).to have_link(href: logout_path, count: 0)
-    expect(page).to have_link(href: user_path(user), count: 0)
+  describe 'visit users_path' do
+    scenario 'should have the proper link as login user' do
+      user = create(:user)
+      # make many users for multiple pagination
+      100.times do |i|
+        create(:user, email: "example-#{i}@example.com")
+      end
+
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_button 'Log in'
+      expect(current_path).to eq(user_path(user))
+      visit users_path
+      # is exist pagination
+      # in the logo and the left of the header
+      expect(page).to have_link href: root_path, count: 2
+      # in the header
+      expect(page).to have_link href: help_path
+      # in the footer
+      expect(page).to have_link href: about_path
+      # in the footer
+      expect(page).to have_link href: contact_path
+      expect(page).to have_selector('div.pagination', count: 2)
+
+      User.paginate(page: 1).each do |u|
+        expect(page).to have_selector('ul>li>img.gravatar')
+        expect(page).to have_link href: user_path(u), text: u.name
+      end
+    end
+
+    scenario 'should redirect to login_url as non-login user' do
+      user = create(:user)
+      # without log-in
+      visit users_path
+
+      expect(current_path).to eq login_path
+    end
   end
 end
