aa3901e4ae65ddcbc78b21982f0dd20a9a5af73a
knknkn1162 <knknkn1162@gmail.com>
2018-01-17 11:09:12 +0900

    set has_many to microposts & model test

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation test' do
+    it 'shold be valid' do
+      user = build_stubbed(:user)
+      micropost = user.microposts.build(content: 'Lorem ipsum')
+      expect(micropost).to be_valid
+    end
+
+    it 'user_id should be present' do
+      user = build_stubbed(:user)
+      micropost = user.microposts.build(user_id: nil)
+      expect(micropost).not_to be_valid
+    end
+
+    it 'should have non-empty content' do
+      user = build_stubbed(:user)
+      micropost = user.microposts.build(content: ' ')
+      expect(micropost).not_to be_valid
+    end
+
+    it 'should have content less than or equal 140 characters' do
+      user = build_stubbed(:user)
+      micropost = user.microposts.build(content: 'a'*141)
+      expect(micropost).not_to be_valid
+    end
+  end
 end
