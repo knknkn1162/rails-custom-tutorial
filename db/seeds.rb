62976e4d72ca2503361cd19ce9bc6ffda2ee7f54
knknkn1162 <knknkn1162@gmail.com>
2018-01-17 23:22:15 +0900

    add seeds & css

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,8 @@ User.create(
     activated_at: Time.zone.now
   )
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  users.each{ |user| user.microposts.create!(content: Faker::Lorem.sentence(5)) }
+end
