9e1d33585fed56a7a4e35447609d7a6f8fb32a87
knknkn1162 <knknkn1162@gmail.com>
2018-01-19 12:35:21 +0900

    impl image uploader & test features

--- spec/features/microposts_spec.rb
+++ spec/features/microposts_spec.rb
@@ -16,6 +16,7 @@ RSpec.feature "Microposts", type: :feature do
     scenario 'login with valid information' do
       user = home_with_log_in
       expect(page).to have_selector('div.pagination')
+      expect(page).to have_selector('input[type=file]')
 
       fill_in 'micropost_content', with: 'The sample content'
       click_button 'Post'
@@ -44,8 +45,11 @@ RSpec.feature "Microposts", type: :feature do
       expect(page).to have_content '0 microposts'
 
       fill_in 'micropost_content', with: 'sample content'
+      attach_file 'micropost_picture', 'spec/fixtures/rails.png'
       click_button 'Post'
       expect(page).to have_content '1 micropost'
+      # check img posted
+      expect(page).to have_selector('span.content img', count: 1)
     end
   end
 end
