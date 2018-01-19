630b6f68aae7ba60dee8c1256cf4811190977055
knknkn1162 <knknkn1162@gmail.com>
2018-01-19 15:03:54 +0900

    validate on Relationship & test model

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'user validation test' do
+    it 'should be valid' do
+      relationship = build(:relationship)
+      expect(relationship).to be_valid
+    end
+
+    it 'should require a follower id' do
+      relationship = build(:relationship, follower_id: nil)
+      expect(relationship).not_to be_valid
+    end
+
+    it 'should require a followed id' do
+      relationship = build(:relationship, followed_id: nil)
+      expect(relationship).not_to be_valid
+    end
+  end
 end
